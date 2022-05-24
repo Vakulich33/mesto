2154a934c5dac3f7bbcf3ce89c7eb09438d2559d
ilya Vakulich <ilyavakulich3301@yandex.ru>
2022-05-24 21:03:14 +0300

    fix bugs

--- scripts/index.js
+++ scripts/index.js
@@ -12,16 +12,16 @@ const modalWindowAdd = document.querySelector(".popup_add");
 const closeAddButton = document.querySelector(".popup__close_add");
 const namePlace = document.querySelector(".popup__name_add");
 const linkPlace = document.querySelector(".popup__description_add")
-const popupImage = document.querySelector(".popup__image");
+const popupImage = document.querySelector(".popup_image");
 const deleteButton = document.querySelector(".profile__trash-button");
 const placeTemple = document.querySelector(".place-template");
 const emptyHeart = document.querySelectorAll(".place__heart");
 const ulList = document.querySelector(".places");
 const submitFormAdd = document.querySelector(".popup__form_add");
-const popupImageText = document.querySelector(".popup__image-text")
-const popupImagePic = document.querySelector(".popup__image-picture");
+const popupImageText = document.querySelector(".popup_image-text")
+const popupImagePic = document.querySelector(".popup_image-picture");
 const closeButtonImage = document.querySelector(".popup__close_image");
-const imageText = document.querySelector(".popup__image-text");
+const imageText = document.querySelector(".popup_image-text");
 const initialCards = [
   {
     name: 'Архыз',
@@ -83,7 +83,7 @@ function getCard(item) {
   image.addEventListener("click", (evt) => {
   openPopup(popupImage)
   popupImagePic.src = `${evt.target.src}`;
-  imageText.textContent = `${evt.target.alt}`;
+  imageText.textContent = `${evt.target.text}`;
   popupImagePic.alt = `${evt.target.alt}`;
    });
   ulList.prepend(placeElement);
